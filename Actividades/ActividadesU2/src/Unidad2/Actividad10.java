f891946a2175280bee22b9dc348db1f5c14b85d9
Germán <lledo.pena.german@iescamas.es>
2025-09-29 18:40:32 +0200

    actividad10 ud2 añadida

--- Actividades/ActividadesU2/src/Unidad2/Actividad10.java
+++ Actividades/ActividadesU2/src/Unidad2/Actividad10.java
@@ -5,17 +5,55 @@ import java.util.Scanner;
 public class Actividad10 {
 
 	public static void main(String[] args) {
-		var sc = new Scanner(System.in);
 		
-		//Pedimos el numero
+        /*
+         * PEDIR UN NUMERO ENTRE 0 Y 9999 Y DECIR SI ES CAPICUA
+         */
+        var sc = new Scanner(System.in);
+
+        int unidades, decenas, centenas, millares;
+        final String capicua = "Es Capicua";
+        final String noCapicua = "No es Capicua";
+
+        // Pedimos el numero
         System.out.print("Introduce un numero (0 y 9.999): ");
         int n = sc.nextInt();
-        
-        if (n >= 0 && n < 9999) {
-        	
-        }
-        
 
-        sc.close();
+        if (n < 10) {
+            System.out.print("No es capicua (Solo una cifra)");
+        } else if (n < 100) {
+            unidades = n % 10;
+            decenas = n / 10;
+
+            if (unidades == decenas) {
+                System.out.print(capicua);
+            } else {
+                System.out.print(noCapicua);
+            }
+        } else if (n < 1000) {
+            unidades = n % 10;
+            centenas = n / 100;
+
+            if (unidades == centenas) {
+                System.out.print(capicua);
+            } else {
+                System.out.print(noCapicua);
+            }
+        } else if (n < 10000) {
+            unidades = n % 10;
+            decenas = (n / 10) % 10;
+            centenas = (n / 100) % 10;
+            millares = n / 1000;
+
+            if (unidades == millares && decenas == centenas) {
+                System.out.print(capicua);
+            } else {
+                System.out.print(noCapicua);
+            }
+        } else {
+            System.out.println("Número fuera de rango.");
+        }
+
+        sc.close();       
 	}
 }
